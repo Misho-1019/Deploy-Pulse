c4b71cfda1efa1661461b9f7c64d52b396595121
Misho1019 <mikailtorres99@gmail.com>
2026-06-20 23:19:54 +0300

    Remove mode='wait' from AnimatePresence to prevent white screen on page transitions

--- frontend/src/components/layouts/AppLayout.tsx
+++ frontend/src/components/layouts/AppLayout.tsx
@@ -45,7 +45,7 @@ export default function AppLayout() {
         )}
       </header>
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        <AnimatePresence mode="wait">
+        <AnimatePresence>
           <motion.div
             key={location.pathname}
             initial={{ opacity: 0, y: 8 }}
